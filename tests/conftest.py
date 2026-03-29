5abd11f8968c38c8cdc113b21d8e65357b7af6a9
d.erastov@mail.ru <d.erastov@mail.ru>
2026-03-29 11:51:25 +0300

    add allur reports, add fixture for screenshot

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,24 @@
 import pytest
+import allure
+
 from pages.login_page import LoginPage
 
 @pytest.fixture
 def login_page(page):
     # Создаем объект страницы один раз для всех тестов
     return LoginPage(page)
+
+
+
+# Эта фикстура будет срабатывать после каждого теста
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == 'call' and rep.failed:
+        page = item.funcargs['page']
+        allure.attach(
+            page.screenshot(),
+            name="screenshot",
+            attachment_type=allure.attachment_type.PNG
+        )
