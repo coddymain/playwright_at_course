e92df1cc9447e1f0bccc0185f380db55e4985427
d.erastov@mail.ru <d.erastov@mail.ru>
2026-03-31 23:55:19 +0300

    add .env

--- tests/test_heroku_auth.py
+++ tests/test_heroku_auth.py
@@ -1,50 +1,29 @@
-from playwright.sync_api import Page, expect
+import os
+from dotenv import load_dotenv
+from playwright.sync_api import expect
+import pytest
 
+# 1. Загружаем секреты из .env
+load_dotenv()
 
-# В Pytest мы просто передаем 'page' в аргументы. 
-# Pytest сам откроет браузер, создаст страницу и закроет её после теста!
-def test_successful_login(page: Page):
-    # 1. Переходим на страницу
-    page.goto("https://the-internet.herokuapp.com/login")
-        
-    # 2. Вводим данные (используем локаторы из твоего черновика)
-    page.get_by_role("textbox", name="Username").fill("tomsmith")
-    page.get_by_role("textbox", name="Password").fill("SuperSecretPassword!")
+def test_successful_login(login_page):
+    """Тест успешного входа с использованием скрытых учетных данных"""
+    # Берем данные из файла .env
+    user = os.getenv("HEROKU_USER")
+    password = os.getenv("HEROKU_PASSWORD")
     
-    # 3. Нажимаем кнопку Login
-    page.get_by_role("button", name=" Login").click()
+    login_page.navigate()
+    login_page.login(user, password)
     
-    # 4. ПРОФЕССИОНАЛЬНЫЙ ШАГ: Проверка (Assertion)
-    # Мы ожидаем, что на странице появится текст об успешном входе
-    expect(page.get_by_text("You logged into a secure area!")).to_be_visible()
+    expect(login_page.flash_message).to_contain_text("You logged into a secure area!")
 
+@pytest.mark.parametrize("username, password, expected_error", [
+    ("invalid_user", "wrong_pass", "Your username is invalid!"),
+    ("tomsmith", "wrong_password", "Your password is invalid!"),
+])
+def test_negative_logins(login_page, username, password, expected_error):
+    """Параметризованный тест для проверки ошибок входа"""
+    login_page.navigate()
+    login_page.login(username, password)
     
-    # Нажимаем на кнопку выхода, которую мы только что обсудили
-    page.get_by_role("link", name="Logout").click()
-    # Проверяем, что снова видим заголовок страницы входа
-    expect(page.get_by_role("heading", name="Login Page")).to_be_visible()
-    
-def test_failed_login(page: Page):
-    # 1. Переходим на страницу
-    page.goto("https://the-internet.herokuapp.com/login")
-
-    # 2. Вводим неправильные данные
-    page.get_by_role("textbox", name="Username").fill("wrong_user")
-    page.get_by_role("textbox", name="Password").fill("wrong_password")
-    # 3. Нажимаем кнопку Login
-    page.get_by_role("button", name=" Login").click()
-    # 4. Проверяем, что отображается сообщение об ошибке
-    # Мы используем id="flash" для локатора, так как это уникальный идентификатор для этого сообщения
-    error_message = page.locator("#flash")
-    expect(error_message).to_contain_text("Your username is invalid!")
-        
-def test_empty_fields_login(page: Page):
-    # 1. Переходим на страницу
-    page.goto("https://the-internet.herokuapp.com/login")
-
-    # 2. Оставляем поля пустыми и нажимаем кнопку Login
-    page.get_by_role("button", name=" Login").click()
-    
-    # 3. Проверяем, что отображается сообщение об ошибке для пустых полей
-    error_message = page.locator("#flash")
-    expect(error_message).to_contain_text("Your username is invalid!") 
+    expect(login_page.flash_message).to_contain_text(expected_error)
